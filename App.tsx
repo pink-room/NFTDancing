86ff0891039176f12f5fce0c7e028a5330f330b6
André Maximino <andrefbmaximino@gmail.com>
2022-07-29 01:46:54 +0100

    Remove warning logs

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {LogBox} from 'react-native';
 
 import {AuthContextProvider} from './src/contexts/AuthContext';
 import {Web3ContextProvider} from './src/contexts/Web3Context';
@@ -11,6 +12,10 @@ const {
 const {withWalletConnect} = require('@walletconnect/react-native-dapp');
 
 function App() {
+    useEffect(() => {
+        LogBox.ignoreAllLogs();
+    }, []);
+
     return (
         <AuthContextProvider>
             <Web3ContextProvider>
